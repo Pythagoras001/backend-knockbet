8827df42e35c281dd92f2bf942bad1a4fa6d89d5
PythagorasGit <thomas.gomezr@upb.edu.co>
2026-05-07 09:54:03 -0500

    chore: add initial project dependencies

---
+++ src/main/java/com/knockbet/backend_knockbet/BackendKnockbetApplication.java
@@ -0,0 +1,13 @@
+package com.knockbet.backend_knockbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKnockbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKnockbetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knockbet/backend_knockbet/BackendKnockbetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knockbet.backend_knockbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKnockbetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
